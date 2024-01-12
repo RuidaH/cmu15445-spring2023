5c1efaf768908ff6453f3219e64ef58b88f5fc13
underdog24 <ReeseH.life@gmail.com>
2024-01-12 19:50:36 +0800

    proj3 更新

--- bustub-20230514-2023spring/src/execution/insert_executor.cpp
+++ bustub-20230514-2023spring/src/execution/insert_executor.cpp
@@ -18,10 +18,35 @@ namespace bustub {
 
 InsertExecutor::InsertExecutor(ExecutorContext *exec_ctx, const InsertPlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void InsertExecutor::Init() { throw NotImplementedException("InsertExecutor is not implemented"); }
+// acquire table_info_ and index_info_
+void InsertExecutor::Init() {
+    auto catalog = exec_ctx_->GetCatalog();
+    table_info_ = catalog->GetTable(plan_->TableOid());  // table_info_->table_
+    indexes_info_ = catalog->GetTableIndexes(table_info_->name_);
+}
 
-auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
+// before insert, make sure the table exists in the database
+auto InsertExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+
+    // 这里还漏了 child_executor 可能会产出多条 inserted tuples
+
+    // acquire the inserted tuple from the child_executor_
+    child_executor_->Next(tuple, rid);
+
+    // insert the tuple and tuple_meta into the table
+    TupleMeta tuple_meta{INVALID_TXN_ID, INVALID_TXN_ID, false};
+    *rid = table_info_->table_->InsertTuple(tuple_meta, *tuple, nullptr, nullptr, table_info_->oid_);
+
+    // update the index if there is one
+    for (auto index_info : indexes_info_) {
+        if (!index_info->index_->InsertEntry(*tuple, *rid, nullptr)) {
+            return false;
+        }
+    }
+    
+    return true;
+}
 
 }  // namespace bustub
--- bustub-20230514-2023spring/src/execution/seq_scan_executor.cpp
+++ bustub-20230514-2023spring/src/execution/seq_scan_executor.cpp
@@ -14,10 +14,32 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {
+    auto catalog = exec_ctx_->GetCatalog();
+    auto table_info = catalog->GetTable();
+    // 这里返回的是初始化列表, 所以需要使用指针
+    iter_ = new TableIterator(table_info->table_->MakeIterator());
+}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+// next tuple and the corresponding rid should be assigned to the param
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+    // keep iterating through the table until you find a non-deleted tuple
+    while (true) {
+        if (iter_->IsEnd()) {
+            return false;
+        }
+
+        if (!(*iter_)->GetTuple().first.is_deleted_) {
+            *tuple = (*iter_)->GetTuple();
+            *rid = (*iter_)->GetRID();
+            ++(*iter_);
+            return true;
+        }
+
+        ++(*iter_);
+    }
+}
 
 }  // namespace bustub
